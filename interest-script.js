68e13acfb6966b3cf835107d03a7965b358bc2b9
Hiimaanshi dogra <60709487+himanshi33@users.noreply.github.com>
2021-02-22 16:56:09 +0530

    Create interest-script.js

---
+++ interest-script.js
@@ -0,0 +1,14 @@
+function dochange(){
+alert('are you sure you want to make following changes? with js function');
+ }
+function changecolor(){
+  var dd1=document.getElementById("d1");
+  var dd2=document.getElementById("d2");
+dd1.className="redBack";
+  dd2.className="greenBack";
+}
+function changetext(){
+  var dd1=document.getElementById("d1");
+  var dd2=document.getElementById("d2");
+   dd1.innerHTML="BONJOUR";
+  dd2.innerHTML="SAYONARA";}
